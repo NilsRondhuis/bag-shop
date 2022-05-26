58b4d92b237d07538cf63a8d148c011b9e01f685
NilsRondhuis <nilsrondhuis@ukr.net>
2022-05-27 00:12:28 +0300

    first commit

---
+++ swiper-slider.js
@@ -0,0 +1,20 @@
+new Swiper(".characteristics-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  simulateTouch: true,
+  grabCursor: true,
+  loop: true,
+});
+
+new Swiper(".reviews-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  simulateTouch: true,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 30,
+});
